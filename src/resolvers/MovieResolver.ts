83e9b55c6b0fffb0d24296b94022d5de6b9d5e92
Alex Nedelkovski <ace_nedelkovski@hotmail.com>
2019-11-18 18:46:31 -0600

    update works

--- src/resolvers/MovieResolver.ts
+++ src/resolvers/MovieResolver.ts
@@ -6,18 +6,35 @@ class MovieInput {
   @Field()
   title: string
 
-  @Field(() => Int )
+  @Field(() => Int)
   minutes: number
+}
+
+@InputType()
+class MovieUpdateInput {
+  @Field(() => String, { nullable: true })
+  title?: string
+
+  @Field(() => Int, { nullable: true })
+  minutes?: number
 }
 
 @Resolver()
 export class MovieResolver {
   @Mutation(() => Movie)
   async createMovie(
-    @Arg("options", ()=> MovieInput) options: MovieInput,
+    @Arg("options", () => MovieInput) options: MovieInput,
   ) {
     const movie = await Movie.create(options).save();
     return movie;
+  }
+
+  @Mutation(() => Boolean)
+  async updateMovie(
+    @Arg("id") id: number,
+    @Arg("input", () => MovieUpdateInput) input: MovieUpdateInput) {
+    await Movie.update({ id }, input)
+    return true
   }
 
   @Query(() => [Movie])
